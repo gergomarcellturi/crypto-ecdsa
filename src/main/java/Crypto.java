f172cf771995ecb54a7abffed1b681b45518b1bb
Turi Gergő Marcell <turi.gergo.marcell@udinfopark.hu>
2021-11-29 19:48:48 +0100

    ready again

--- src/main/java/Crypto.java
+++ src/main/java/Crypto.java
@@ -10,7 +10,7 @@ public class Crypto {
 
         Signature signature = ecdsa.sign("Hello World");
         System.out.println(ecdsa.verify("Hello World", signature));
-        System.out.println(ecdsa.verify("Hello Worfld", signature));
+        System.out.println(ecdsa.verify("Hello World Wrong", signature));
         System.out.println(ecdsa.verify("Hello World", signature));
 
     }
